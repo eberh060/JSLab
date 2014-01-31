ccc617b989137441bd26a40a6a0a52b3539750ef
eberh060 <eberh060@morris.umn.edu>
2014-01-31 16:15:08 -0600

    Added buttons and jquery to font size, color, background, etc. Work in progress for Search and undo redo save.

--- Functions.js
+++ Functions.js
@@ -1,9 +1,7 @@
 /**
  * Created by eberh060 on 1/28/14.
  */
-function countChars(content) {
-    return content.length;
-}
+
 
 function searchFor(what, where) { <!--returns the index-->
     return where.indexOf(what);
@@ -26,12 +24,14 @@ function makeObject(start){
             saveStates.add(newState);
         }
     }
+    function getState(){
+        return saveStates[saveStates.length - 1];
+    }
 }
 
-function highlight(search){
-    return "$('body').text().highlight('" + search + "my text to highlight')";
-}
+//function highlight(search){
+//    return "$('body').text().highlight('" + search + "my text to highlight')";
+//}
 
-module.exports.countChars = countChars;
 module.exports.searchFor = searchFor;
-module.exports.highlight = highlight;
+//module.exports.highlight = highlight;
